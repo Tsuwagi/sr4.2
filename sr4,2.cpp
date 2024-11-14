00eb527007de247ee3768c96fed7b55b7527a952
Tsuwagi <viktoria16kutas5@gmail.com>
2024-11-14 12:21:09 +0000

    first version

--- sr4,2.cpp
+++ sr4,2.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int a, b, op;
+    double result;
+
+    cout << "first\n";
+    cin >> a;
+
+    cout << "Op\n1\t+\n2\t-\n3\t*\n4\t/\n5\t^\n\n";
+    cin >> op;
+
+    cout << "second\n";
+    cin >> b;
+
+    switch (op) {
+    case 1:
+        result = a + b;
+        break;
+    case 2:
+        result = a - b;
+        break;
+    case 3:
+        result = a * b;
+        break;
+    cout << " result = " << result << endl;
+return 0;
+}
 
